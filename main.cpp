43c87f8109d925e7317f83e4f6386838f2179cdf
Olimpia Gawrońska <gawronskaolimpia@gmail.com>
2024-10-18 11:26:17 +0200

    Generowanie DNA
    FUnckja od generowania DNA only

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+string generateDNA(int n) {
+    string DNA;
+for (int i = 0; i < n; i++) {
+
+    const char nucleotides[] = {'A', 'C', 'T', 'G'};
+    char generatedNucleotide = nucleotides[rand() % 4];
+    DNA += generatedNucleotide;
+}
+return DNA;
+}
 
 int main() {
-    std::cout << "Rówież Cię witam :3 Hello!" << std::endl;
+    int n,k; // n - długosć łańcucha , k - długość oligonukleotydów
+    string DNA;
+    vector<string> idealSpectrum;
+    cout << "Podaj długość łańcucha: "<<endl;
+    cin >> n;
+    cout << "Podaj długosć oligonukleotydów:" << endl;
+    cin >> k;
+
+   DNA =  generateDNA(n);
+
+    cout << DNA << endl;
     return 0;
 }
