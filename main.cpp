b3b84d87c323b28435aa08d3e1757dd7c3ded6b6
Olimpia Gawrońska <gawronskaolimpia@gmail.com>
2024-10-18 11:41:36 +0200

    Idealne spektrum
    no comment

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
@@ -17,17 +18,34 @@ for (int i = 0; i < n; i++) {
 return DNA;
 }
 
+vector<string> generateIdealSpectrum(int k, int n, string DNA) {
+    vector<string> idealSpectrum;
+    for (int i = 0; i <n-k+1; i++) {
+        string oligonucleotide = DNA.substr(i, k);
+        idealSpectrum.push_back(oligonucleotide);
+
+    }
+        return idealSpectrum;
+    }
+
 int main() {
+    srand(static_cast<unsigned>(time(0)));
     int n,k; // n - długosć łańcucha , k - długość oligonukleotydów
-    string DNA;
+    string DNA, primer;
     vector<string> idealSpectrum;
     cout << "Podaj długość łańcucha: "<<endl;
     cin >> n;
     cout << "Podaj długosć oligonukleotydów:" << endl;
     cin >> k;
 
-   DNA =  generateDNA(n);
+    DNA =  generateDNA(n);
+    idealSpectrum = generateIdealSpectrum(k, n, DNA);
+    primer = idealSpectrum[0];
 
     cout << DNA << endl;
+    cout << primer << endl;
+    for (string element : idealSpectrum) {
+        std::cout << element << " ";  // Wyświetlamy każdy element
+    }
     return 0;
 }
